e330f090be945ab1f26f6012d3ae000d03004835
Aman Thakur <builtbyaman@gmail.com>
2023-03-03 22:44:25 +0530

    commit

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 public class Example {
   public static void main(String[] args) {
     System.out.println("We are making PEZ Dispenser");
-    PezDispenser dispenser = new PezDispenser();
+    PezDispenser dispenser = new PezDispenser("Yoda");
     System.out.printf("The dispenser is %s \n", dispenser.getCharacterName());
   }
 }
--- PezDispenser.java
+++ PezDispenser.java
@@ -1,5 +1,19 @@
 class PezDispenser {
-  private String characterName = "Yoda"; // Encapsulation
+  public static final int MAX_PEZ = 12;
+  // static = available to class before initialization. Like Integer.parseInt()
+  final private String characterName;
+  // Final = assign once only
+  private int pezCount;
+
+  // Constructor class
+  public PezDispenser(String characterName) {
+    this.characterName = characterName;
+    pezCount = 0;
+  }
+
+  public void fill() {
+    this.pezCount = MAX_PEZ;
+  }
 
   public String getCharacterName() {
     return characterName;
